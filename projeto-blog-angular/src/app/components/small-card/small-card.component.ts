f66716692a8c050aad4e3c40ea51966cbde02f9d
ivina albuquerque <ivina.albuquerque.s@gmail.com>
2023-05-13 15:02:33 -0300

    conclusão componentes e estilos

--- projeto-blog-angular/src/app/components/small-card/small-card.component.ts
+++ projeto-blog-angular/src/app/components/small-card/small-card.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  ngOnInit(): void {}
 }
